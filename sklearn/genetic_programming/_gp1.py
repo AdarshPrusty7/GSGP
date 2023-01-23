687ca7337c45ad033e55522bb8b5ccc57639b681
AdaPru <adarshprusty2002@gmail.com>
2023-01-23 20:56:09 +0000

    Skeleton for traditional GP

---
+++ sklearn/genetic_programming/_gp1.py
@@ -0,0 +1,24 @@
+# Author: Adarsh Prusty
+
+class GeneticProgram:
+    def __init__(self) -> None:
+        pass
+
+    def fitness(self):
+        pass
+
+    def selection(self):
+        pass
+
+    def crossover(self):
+        pass
+
+    def mutation(self):
+        pass
+
+    def population_evolution(self):
+        pass
+
+    def hill_climbing(self):
+        pass
+
