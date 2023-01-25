95c09a4f408ee1225e04a61c374e8bbe5a6ef810
AdaPru <adarshprusty2002@gmail.com>
2023-01-25 11:24:05 +0000

    Initial Program Creation

--- sklearn/genetic_programming/_base.py
+++ sklearn/genetic_programming/_base.py
@@ -19,26 +19,26 @@ class BooleanPopulation:
     def __init__(self):
         self.population = []
 
-    def __create_boolean_expression(self, depth, vars):
+    def create_boolean_expression(self, depth, vars):
         'Create a random Boolean expression using recursion.'
         if depth == 1 or random.random() < 1.0 / (2 ** depth - 1):
             return random.choice(vars)
         if random.random() < 1.0 / 3:
-            return 'not' + ' ' + self.__create_boolean_expression(depth - 1, vars)
+            return 'not' + ' ' + self.create_boolean_expression(depth - 1, vars)
         else:
-            return '(' + self.__create_boolean_expression(depth - 1, vars) + ' ' + random.choice(
-                ['and', 'or']) + ' ' + self.__create_boolean_expression(depth - 1, vars) + ')'
+            return '(' + self.create_boolean_expression(depth - 1, vars) + ' ' + random.choice(
+                ['and', 'or']) + ' ' + self.create_boolean_expression(depth - 1, vars) + ')'
 
     def __create_boolean_function(self, depth, vars):
         'Create a random Boolean function.'
-        expression = self.__create_boolean_expression(depth, vars)
+        expression = self.create_boolean_expression(depth, vars)
         boolean_function = eval('lambda ' + ', '.join(vars) + ': ' + expression)  # create function of n input variables
         boolean_function = memoize(boolean_function)  # add cache to the function
         boolean_function.genotype = lambda: expression  # store genotype within function
         return boolean_function
 
-    def create_boolean_population(self, depth, vars, population_size):
-        'Create population of Boolean functions.d'
+    def create_boolean_function_population(self, depth, vars, population_size):
+        'Create population of Boolean functions.'
         self.population = [self.__create_boolean_function(depth, vars) for _ in range(population_size)]
 
     def get_boolean_population(self):
@@ -82,19 +82,28 @@ class ProgramPopulation:
 
     def __create_program_expression(self, depth, vars):
         'Create a random program expression using recursion.'
-        pass
+        if depth == 1 or random.random() < 1.0 / (2 ** depth - 1):
+            return random.choice(vars)
+        else:
+            return '(if ' + BooleanPopulation.create_boolean_expression(depth - 1,
+                                                             vars) + ' then ' + self.__create_program_expression(
+                depth - 1, vars) + ' else ' + self.__create_program_expression(depth - 1, vars) + ')'
 
     def __create_program_function(self, depth, vars):
         'Create a random program function.'
-        pass
+        expression = self.__create_program_expression(depth, vars)
+        program_function = eval('lambda ' + ', '.join(vars) + ': ' + expression)  # create function of n input variables
+        program_function = memoize(program_function)  # add cache to the function
+        program_function.genotype = lambda: expression  # store genotype within function
+        return program_function
 
     def create_program_population(self, depth, vars, population_size):
         'Create population of program functions.'
-        pass
+        self.population = [self.__create_program_function(depth, vars) for _ in range(population_size)]
 
     def get_program_population(self):
         'Return population of program functions.'
-        pass
+        return self.population
 
 
 vars = ['x0', 'x1', 'x2', 'x3', 'x4', 'x5']
@@ -106,3 +115,7 @@ print([p.genotype() for p in bool.get_boolean_population()])
 arith = ArithmeticPopulation()
 arith.create_arithmetic_population(3, vars, 3)
 print([p.genotype() for p in arith.get_arithmetic_population()])
+
+prog = ProgramPopulation()
+prog.create_program_population(3, vars, 3)
+print([p.genotype() for p in prog.get_program_population()])
--- sklearn/genetic_programming/_gp1.py
+++ sklearn/genetic_programming/_gp1.py
@@ -1,23 +1,46 @@
 # Author: Adarsh Prusty
 
 class GeneticProgram:
-    def __init__(self) -> None:
+    def __init__(self, input_data, output_data, GENERATIONS=10, POPULATION_SIZE=100, NUMVARS) -> None:
+        self.input_data = input_data
+        self.output_data = output_data
+        self.population = []
+        self.GENERATIONS = GENERATIONS
+        self.POPULATION_SIZE = POPULATION_SIZE
+        self.NUMVARS = NUMVARS
+
+    def fitness(self, individual):
+        """Fitness function"""
+        fitness = 0
+        for i in range(len(self.input_data)):
+            if individual(self.input_data[i]) == self.output_data[i]:
+                fitness += 1
+        return fitness
+
+    def selection(self, population):
+        """Roulette Wheel Selection"""
+
+    def crossover(self, population):
         pass
 
-    def fitness(self):
-        pass
-
-    def selection(self):
-        pass
-
-    def crossover(self):
-        pass
-
-    def mutation(self):
+    def mutation(self, population):
         pass
 
     def population_evolution(self):
-        pass
+        """Population Based Evolution"""
+        population = self.population
+
+        for i in range(self.GENERATIONS):
+            graded_population = [(self.fitness(individual), individual) for individual in population]
+            selected_population = self.selection(graded_population)
+            crossover_population = self.crossover(selected_population)
+            mutated_population = self.mutation(crossover_population)
+            population = mutated_population
+
+        graded_population = [(self.fitness(individual), individual) for individual in population]
+        graded_population.sort(key=lambda x: x[0])
+        all_true_individual = graded_population[0](*([True] * self.NUMVARS))
+        return graded_population[0], all_true_individual
 
     def hill_climbing(self):
         pass
