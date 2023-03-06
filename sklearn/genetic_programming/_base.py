73c603c77218e190ffbd5c9594042bf8c063672c
AdarshPrusty7 <ap910@exeter.ac.uk>
2023-03-06 19:26:39 +0000

    Changes and Init Bool
    Creates changes in _base.py and creates an initial boolean subclass

--- sklearn/genetic_programming/_base.py
+++ sklearn/genetic_programming/_base.py
@@ -1,7 +1,7 @@
 
 import random
 
-def __memoize(f):
+def memoize(f):
     'Add a cache memory to the input function.'
     f.cache = {}
     def decorated_function(*args):
@@ -23,19 +23,19 @@ class BooleanPopulation:
         if random.random() < 1.0 / 3:
             return 'not' + ' ' + self.__create_boolean_expression(depth - 1, vars)
         else:
-            return '(' + self.__create_boolean_expression(depth - 1, vars) + ' ' + random.choice(['and', 'or']) + ' ' + self.__create_boolean_expression(depth - 1) + ')'
+            return '(' + self.__create_boolean_expression(depth - 1, vars) + ' ' + random.choice(['and', 'or']) + ' ' + self.__create_boolean_expression(depth - 1, vars) + ')'
 
-    def __create_boolean_function(self, depth, vars):
+    def create_boolean_function(self, depth, vars):
         'Create a random Boolean function.'
         expression = self.__create_boolean_expression(depth, vars)
         boolean_function = eval('lambda ' + ', '.join(vars) + ': ' + expression)  # create function of n input variables
-        boolean_function = __memoize(boolean_function)  # add cache to the function
+        boolean_function = memoize(boolean_function)  # add cache to the function
         boolean_function.genotype = lambda: expression  # store genotype within function
         return boolean_function
 
     def create_boolean_population(self, depth, vars, population_size):
         'Create population of Boolean functions.d'
-        self.population = [self.__create_boolean_function(depth, vars) for _ in range(population_size)]
+        self.population = [self.create_boolean_function(depth, vars) for _ in range(population_size)]
 
     def get_boolean_population(self):
         'Return population of Boolean functions.'
@@ -47,7 +47,11 @@ class ArithmeticPopulation:
 
     def __create_arithmetic_expression(self, depth, vars):
         'Create a random arithmetic expression using recursion.'
+        if depth == 1 or random.random() < 1.0 / (2 ** depth - 1):
+            return random.choice(vars)
         pass
+
+
 
     def __create_arithmetic_function(self, depth, vars):
         'Create a random arithmetic function.'
@@ -68,7 +72,9 @@ class ProgramPopulation:
 
     def __create_program_expression(self, depth, vars):
         'Create a random program expression using recursion.'
-        pass
+        if depth == 1 or random.random() < 1.0 / (2 ** depth - 1):
+            return random.choice(vars)
+            
 
     def __create_program_function(self, depth, vars):
         'Create a random program function.'
@@ -82,4 +88,8 @@ class ProgramPopulation:
         'Return population of program functions.'
         pass
 
-    
+
+boolean = BooleanPopulation()
+boolean.create_boolean_population(4, ['x1', 'x2', 'x3', 'x4', 'x5'], 5)
+print(boolean.get_boolean_population()[0].genotype())
+print([x.genotype() for x in boolean.get_boolean_population()])
