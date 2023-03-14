9c85574c22b6feb0a73f2d0d0927ef4fb2b3d960
Adarsh <ap910@exeter.ac.uk>
2023-03-14 06:05:06 +0000

    Major changes

--- sklearn/genetic_programming/_base.py
+++ sklearn/genetic_programming/_base.py
@@ -15,9 +15,6 @@ def memoize(f):
 
 
 class BooleanPopulation:
-    def __init__(self):
-        self.population = []
-
     def create_boolean_expression(self, depth, vars):
         'Create a random Boolean expression using recursion.'
         if depth == 1 or random.random() < 1.0 / (2 ** depth - 1):
@@ -27,7 +24,6 @@ class BooleanPopulation:
         else:
             return '(' + self.create_boolean_expression(depth - 1, vars) + ' ' + random.choice(
                 ['and', 'or']) + ' ' + self.create_boolean_expression(depth - 1, vars) + ')'
-
 
     def create_boolean_function(self, depth, vars):
         'Create a random Boolean function.'
@@ -39,55 +35,69 @@ class BooleanPopulation:
 
     def create_boolean_population(self, depth, vars, population_size):
         'Create population of Boolean functions.d'
-        self.population = [self.create_boolean_function(depth, vars) for _ in range(population_size)]
-
-    def get_boolean_population(self):
-        'Return population of Boolean functions.'
-        return self.population
-
+        return [self.create_boolean_function(depth, vars) for _ in range(population_size)]
 
 class ArithmeticPopulation:
-    def __init__(self):
-        self.population = []
-
     def __create_arithmetic_expression(self, depth, vars):
         'Create a random arithmetic expression using recursion.'
         if depth == 1 or random.random() < 1.0 / (2 ** depth - 1):
             return random.choice(vars)
         else:
-            return '(' + self.__create_arithmetic_expression(depth - 1, vars) + ' ' + random.choice(
+            term = '(' + self.__create_arithmetic_expression(depth - 1, vars) + ' ' + random.choice(
                 ['+', '-', '/', '*']) + ' ' + self.__create_arithmetic_expression(depth - 1, vars) + ')'
+            try:
+                eval(term, {f"x{i}": i*0.2 for i in range(len(vars))})
+                return term
+            except ZeroDivisionError:
+                return '(' + self.__create_arithmetic_expression(depth - 1, vars) + ' ' + random.choice(
+                    ['+', '-', '/', '*']) + ' ' + self.__create_arithmetic_expression(depth - 1, vars) + ')'
 
-    def __create_arithmetic_function(self, depth, vars):
+    def __create_arithmetic_mask_expression(self, depth, original_depth):
+        'Create a random arithmetic mask expression using recursion.'
+        if depth == 1 or random.random() < 1.0 / (2 ** depth - 1):
+            return str(round(random.random(), 5))
+        else:
+            if depth == original_depth:
+                eval_term = 2
+                term = 0
+                while eval_term < 0 or eval_term > 1:
+                    term = '(' + self.__create_arithmetic_mask_expression(depth - 1, original_depth) + ' ' + random.choice(['+', '-', '/', '*']) + ' ' + self.__create_arithmetic_mask_expression(depth - 1, original_depth) + ')'
+                    try:
+                        eval_term = eval(term)
+                    except ZeroDivisionError:
+                        eval_term = 2 
+                return term
+            else:
+                return '(' + self.__create_arithmetic_mask_expression(depth - 1, original_depth) + ' ' + random.choice(['+', '-', '/', '*']) + ' ' + self.__create_arithmetic_mask_expression(depth - 1, original_depth) + ')'
+
+    def create_arithmetic_function(self, depth, vars):
         'Create a random arithmetic function.'
         expression = self.__create_arithmetic_expression(depth, vars)
-        arithmetic_function = eval(
-            'lambda ' + ', '.join(vars) + ': ' + expression)  # create function of n input variables
+        arithmetic_function = eval('lambda ' + ', '.join(vars) + ': ' + expression)  # create function of n input variables
         arithmetic_function = memoize(arithmetic_function)  # add cache to the function
         arithmetic_function.genotype = lambda: expression  # store genotype within function
         return arithmetic_function
 
+    def create_arithmetic_mask(self, depth, vars):
+        'Creates a random arithmetic mask function/ constant for crossover with codomain [0,1].'
+        expression = self.__create_arithmetic_mask_expression(depth, depth)
+        mask_function = eval('lambda ' + ', '.join(vars) + ': ' + expression)  # create function of n input variables
+        mask_function = memoize(mask_function)  # add cache to the function
+        mask_function.genotype = lambda: expression  # store genotype within function
+        return mask_function
+
     def create_arithmetic_population(self, depth, vars, population_size):
         'Create population of arithmetic functions.'
-        self.population = [self.__create_arithmetic_function(depth, vars) for _ in range(population_size)]
-
-    def get_arithmetic_population(self):
-        'Return population of arithmetic functions.'
-        return self.population
+        return [self.create_arithmetic_function(depth, vars) for _ in range(population_size)]
 
 
 class ProgramPopulation:
-    def __init__(self):
-        self.population = []
-
     def __create_program_expression(self, depth, vars):
         'Create a random program expression using recursion.'
         if depth == 1 or random.random() < 1.0 / (2 ** depth - 1):
             return random.choice(vars)
         else:
-            return '(if ' + BooleanPopulation.create_boolean_expression(depth - 1,
-                                                             vars) + ' then ' + self.__create_program_expression(
-                depth - 1, vars) + ' else ' + self.__create_program_expression(depth - 1, vars) + ')'
+            return '(' + self.__create_program_expression(depth - 1, vars) + ' if ' + self.__create_program_expression(depth - 1, vars) + ' else ' + self.__create_program_expression(depth - 1, vars) + ')'
 
     def __create_program_function(self, depth, vars):
         'Create a random program function.'
@@ -99,24 +109,22 @@ class ProgramPopulation:
 
     def create_program_population(self, depth, vars, population_size):
         'Create population of program functions.'
-        self.population = [self.__create_program_function(depth, vars) for _ in range(population_size)]
+        return [self.__create_program_function(depth, vars) for _ in range(population_size)]
 
-    def get_program_population(self):
-        'Return population of program functions.'
-        return self.population
 
 
 vars = ['x0', 'x1', 'x2', 'x3', 'x4', 'x5']
 
 bool = BooleanPopulation()
-bool.create_boolean_population(4, vars, 5)
-print([p.genotype() for p in bool.get_boolean_population()])
+pop_bool = bool.create_boolean_population(4, vars, 5)
+print([p.genotype() for p in pop_bool])
 
 arith = ArithmeticPopulation()
-arith.create_arithmetic_population(3, vars, 3)
-print([p.genotype() for p in arith.get_arithmetic_population()])
+pop_arith = arith.create_arithmetic_population(4, vars, 5)
+print([p.genotype() for p in pop_arith])
 
 prog = ProgramPopulation()
-prog.create_program_population(3, vars, 3)
-print([p.genotype() for p in prog.get_program_population()])
+pop_prog = prog.create_program_population(4, vars, 5)
+print([p.genotype() for p in pop_prog])
+
 
--- sklearn/genetic_programming/_gp1.py
+++ sklearn/genetic_programming/_gp1.py
@@ -1,62 +1,137 @@
 
 # Author: Adarsh Prusty
 
-import random
 from _base import *
+
+import itertools
+import math
+import random
+
 
 # Boolean Non-Semantic Genetic Operators
 class GeneticProgram:
-    def __init__(self, numvars, depth, pop_size, generations, trunc, target_output) -> None:
+    def __init__(self, numvars, depth, pop_size, generations, trunc, target_funct) -> None:
         self.numvars = numvars
         self.depth = depth
         self.pop_size = pop_size
         self.generations = generations
         self.trunc = trunc
-        self.target_output = target_output
+        self.target_funct = target_funct
         self.vars = []
         pass
 
     def create_vars(self):
         self.vars = ['x'+str(i) for i in range(self.numvars)]
 
+    #### BOOLEAN ####
     def boolean_crossover(self, f1, f2):
         'Crossover operator.'
         mask = BooleanPopulation().create_boolean_function(self.depth, self.vars)
-        offspring = lambda *x: (p1(*x) and mask(*x)) or (p2(*x) and not mask(*x))
+        offspring = lambda *x: (f1(*x) and mask(*x)) or (f2(*x) and not mask(*x))
         offspring = memoize(offspring)
-        offspring.genotype = lambda: '(('+ f1.genotype(), 'and', mask.genotype() + ') or (' + f2.genotype(), 'and not', mask.genotype() + '))'
+        offspring.genotype = lambda: '(('+ f1.genotype() + ' and ' + mask.genotype() + ') or (' + f2.genotype() + ' and not ' + mask.genotype() + '))'
         return offspring
 
-    def mutation(self, f):
+    def boolean_mutation(self, f):
         'Mutation operator.'
-
+        mintermexpr = ' and '.join([random.choice([x,'not ' + x]) for x in self.vars]) 
+        minterm = eval('lambda ' + ', '.join(self.vars) + ': ' + mintermexpr)
         if random.random() < 0.5:
-            pass
-        return f
+            offspring = lambda *x : f(*x) or minterm(*x)
+            offspring = memoize(offspring)
+            offspring.genotype = lambda: '(' + f.genotype() + ' or ' + mintermexpr + ')'
+        else:
+            offspring = lambda *x: f(*x) and not minterm(*x)
+            offspring = memoize(offspring)
+            offspring.genotype = lambda: '(' + f.genotype() + ' and not ' + mintermexpr + ')'
+        return offspring
 
-    def fitness(self, f):
+    def boolean_fitness(self, f):
         'Fitness function.'
-        return f
+        fitness = 0
+        combination_list = [[True, False] for i in range(self.numvars)]
+        for element in itertools.product(*combination_list):
+            if f(*element) != self.target_funct(*element):
+                fitness += 1
+        return fitness
 
-    def selection(self, f):
-        'Selection operator.'
-        return f
-
-    def population_evolution(self):
+    def boolean_population_evolution(self):
         """Population Based Evolution"""
-        population = self.population
+        bool = BooleanPopulation()
+        population = bool.create_boolean_population(self.depth, self.vars, self.pop_size)
 
-        for i in range(self.GENERATIONS):
-            graded_population = [(self.fitness(individual), individual) for individual in population]
-            selected_population = self.selection(graded_population)
-            crossover_population = self.crossover(selected_population)
-            mutated_population = self.mutation(crossover_population)
-            population = mutated_population
+        for generation in range(self.generations):
+            graded_population = [(self.boolean_fitness(individual), individual) for individual in population]
+            sorted_population = sorted(graded_population, key=lambda x : x[0]) # sorted population by its fitness
+            print ('GENERATION: ' + str(generation + 1) + ' FITNESS: ' + str(self.boolean_fitness(sorted_population[0][1])) + ' AVERAGE FITNESS: ' + str(sum(individual[0] for individual in graded_population) / self.pop_size))
+            new_parents = sorted_population[:int(self.trunc*self.pop_size)]
+            if generation == self.generations - 1:
+                print("Hmmm")
+                break
+            for i in range(self.pop_size):
+                parent = random.sample(new_parents, 2) # picks two random parents
+                population[i] = self.boolean_mutation(self.boolean_crossover(parent[0][1], parent[1][1]))
+        
+        print ("Best individual in the last population: ")
+        #print (sorted_population[0][1].genotype()) # This takes a while
+        print ("Query best individual in last population with all True inputs:")
+        print (sorted_population[0][1](*([True] * self.numvars)))
 
-        graded_population = [(self.fitness(individual), individual) for individual in population]
-        graded_population.sort(key=lambda x: x[0])
-        all_true_individual = graded_population[0](*([True] * self.NUMVARS))
-        return graded_population[0], all_true_individual
+    #### REAL ####
+    def real_crossover(self, f1, f2):
+        'Crossover operator.'
+        mask = ArithmeticPopulation().create_arithmetic_function(self.depth, self.vars)
+        offspring = lambda *x: (f1(*x) * mask(*x)) + (f2(*x) + (1 - mask(*x)))
+        offspring = memoize(offspring)
+        offspring.genotype = lambda: '(('+ f1.genotype() + ' and ' + mask.genotype() + ') or (' + f2.genotype() + ' and not ' + mask.genotype() + '))'
+        return offspring
+
+    def real_mutation(self, f):
+        'Mutation operator.'
+        mutation_step = random.random()
+        random_arithmetic_1 = ArithmeticPopulation().create_arithmetic_function(self.depth, self.vars)
+        random_arithmetic_2 = ArithmeticPopulation().create_arithmetic_function(self.depth, self.vars)
+        offspring = lambda *x : f(*x) + mutation_step * (random_arithmetic_1(*x) - random_arithmetic_2(*x))
+        offspring = memoize(offspring)
+        offspring.genotype = lambda: '(' + f.genotype() + ' + ' + str(mutation_step) + ' * (' + random_arithmetic_1.genotype() + ' - ' + random_arithmetic_2.genotype() + '))'
+        return offspring
+    
+    def real_fitness(self, f, seed):
+        'Fitness function.'
+        generator = random.Random(seed)
+        fitness = 0
+        # Generate a list
+        combination_list = [[random.random(), random.random()] for i in range(self.numvars)]
+        for element in itertools.product(*combination_list):
+            if f(*element) != self.target_funct(*element):
+                fitness += 1
+        return fitness
+
+    def real_population_evolution(self):
+        """Population Based Evolution"""
+        arith = ArithmeticPopulation()
+        population = arith.create_arithmetic_population(self.depth, self.vars, self.pop_size)
+        seed = random.randint(0, 10000000)
+
+        for generation in range(self.generations):
+            graded_population = [(self.real_fitness(individual, seed), individual) for individual in population]
+            sorted_population = sorted(graded_population, key=lambda x : x[0])
+            print ('GENERATION: ' + str(generation + 1) + ' FITNESS: ' + str(self.real_fitness(sorted_population[0][1], seed)) + ' AVERAGE FITNESS: ' + str(sum(individual[0] for individual in graded_population) / self.pop_size))
+            new_parents = sorted_population[:int(self.trunc*self.pop_size)]
+            if generation == self.generations - 1:
+                print("Hmmm")
+                break
+            for i in range(self.pop_size):
+                parent = random.sample(new_parents, 2)
+                population[i] = self.real_mutation(self.real_crossover(parent[0][1], parent[1][1]))
+
+        print ("Best individual in the last population: ")
+        #print (sorted_population[0][1].genotype()) # This takes a while
+        print ("Query best individual in last population with all True inputs:")
+        return sorted_population[0][1](*([True] * self.numvars))
+
+    #### PROGRAM ####
+
 
     def hill_climbing(self):
         pass
@@ -64,4 +139,22 @@ class GeneticProgram:
     def run(self):
         'Run the genetic program.'
         # Create initial population
+        pass
 
+
+
+def target_funct(*args):
+    return args.count(True) % 2 == 1
+
+def arith_target_funct(*args):
+    return sum(args)*2
+
+gp = GeneticProgram(5, 4, 20, 30, 0.5, target_funct)
+gp.create_vars()
+gp.boolean_population_evolution()
+
+print("END OF BOOLEAN")
+
+arith_gp  = GeneticProgram(5, 4, 20, 30, 0.5, arith_target_funct)
+arith_gp.create_vars()
+arith_gp.real_population_evolution()
